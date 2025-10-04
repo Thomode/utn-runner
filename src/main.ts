f89afef88c92b96bf3ab70306cf11f99e097bdc9
Thomode <figueroalopeztomas@gmail.com>
2025-10-04 17:09:02 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Habilitar CORS
+  // Habilitar CORS
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
